5b1fcf00dc1a55a8345e3a5e32308c847bf1b667
Brant Burnett <bburnett@centeredgesoftware.com>
2018-03-15 11:51:38 -0400

    Improve autoscaling handling
    Motivation
    ----------
    An AZRebalance operation may cause an unexpected instance termination.
    
    Mananging swap-rebalance operations is tricky when you can't terminate
    instances with delayed replacement.
    
    Modifications
    -------------
    Disable the AZRebalance process on the autoscaling group.
    
    Set min and max size on the autoscaling group to a wide range, and let
    desired control the number of instances.

--- mds-group/main.tf
+++ mds-group/main.tf
@@ -3,33 +3,33 @@ terraform {
 }
 
 locals {
-	built_installer_url    = "http://packages.couchbase.com/releases/${var.couchbase_version}/couchbase-server-${var.couchbase_edition}-${var.couchbase_version}-centos6.x86_64.rpm"
-	installer_url          = "${coalesce(var.installer_url, local.built_installer_url)}"
+  built_installer_url = "http://packages.couchbase.com/releases/${var.couchbase_version}/couchbase-server-${var.couchbase_edition}-${var.couchbase_version}-centos6.x86_64.rpm"
+  installer_url       = "${coalesce(var.installer_url, local.built_installer_url)}"
 
-	major_version          = "${element(split(".", var.couchbase_version), 0)}"
-	minor_version          = "${element(split(".", var.couchbase_version), 1)}"
+  major_version = "${element(split(".", var.couchbase_version), 0)}"
+  minor_version = "${element(split(".", var.couchbase_version), 1)}"
 }
 
 resource "aws_launch_configuration" "node" {
   count = "${var.node_count > 0 ? 1 : 0}"
 
-  name_prefix                 = "${replace("${var.cluster_name} ${var.name}", " ", "-")}"
-  image_id                    = "${local.ami}"
-  instance_type               = "${var.instance_type}"
-  enable_monitoring           = "${var.detailed_monitoring}"
+  name_prefix       = "${replace("${var.cluster_name} ${var.name}", " ", "-")}"
+  image_id          = "${local.ami}"
+  instance_type     = "${var.instance_type}"
+  enable_monitoring = "${var.detailed_monitoring}"
 
   key_name                    = "${var.key_pair_name}"
   security_groups             = ["${var.security_group_ids}"]
   iam_instance_profile        = "${var.iam_instance_profile}"
   associate_public_ip_address = "${var.topology == "public"}"
-  
-  root_block_device           = ["${var.boot_volume}"]
-  ebs_block_device            = ["${merge(var.data_volume, map("device_name", "/dev/sdb"))}"]
-  ebs_optimized               = "${var.ebs_optimized}"
 
-  placement_tenancy           = "${var.placement_tenancy}"
+  root_block_device = ["${var.boot_volume}"]
+  ebs_block_device  = ["${merge(var.data_volume, map("device_name", "/dev/sdb"))}"]
+  ebs_optimized     = "${var.ebs_optimized}"
 
-  user_data                   = "${data.template_file.user_data.rendered}"
+  placement_tenancy = "${var.placement_tenancy}"
+
+  user_data = "${data.template_file.user_data.rendered}"
 
   lifecycle {
     create_before_destroy = true
@@ -41,9 +41,14 @@ resource "aws_autoscaling_group" "node" {
 
   name_prefix          = "${replace("${var.cluster_name} ${var.name}", " ", "-")}"
   launch_configuration = "${aws_launch_configuration.node.name}"
-  min_size             = "${var.node_count}"
-  max_size             = "${var.node_count}"
+  desired_capacity     = "${var.node_count}"
+  min_size             = 1
+  max_size             = 100
   vpc_zone_identifier  = ["${var.subnet_ids}"]
+
+  # Prevent AZ imbalance from resulting in an unexpected termination
+  # It needs to be a manual process with rebalances in Couchbase
+  suspended_processes = ["AZRebalance"]
 
   # Don't wait for instances to start
   # This allows the use of https://github.com/brantburnett/terraform-aws-autoscaling-route53-srv
@@ -66,22 +71,22 @@ data "template_file" "user_data" {
   template = "${file("${path.module}/userdata.tpl.sh")}"
 
   vars {
-    region                           = "${data.aws_region.current.name}"
-    apply_updates                    = "${var.apply_updates ? "echo \"Applying updates...\"\nyum update -y": "echo \"Skipping updates\""}"
-    installer_url                    = "${local.installer_url}"
-    couchbase_edition                = "${var.couchbase_edition}"
-    
-    topology                         = "${var.topology}"
-    cluster_name                     = "${var.cluster_name}"
-    cluster_name_init                = "${local.major_version >= 5 ? var.cluster_name : ""}"
-    cluster_admin_username           = "${var.cluster_admin_username}"
-    cluster_admin_password           = "${var.cluster_admin_password}"
-    index_storage                    = "${var.cluster_index_storage}"
-    data_ramsize                     = "${var.cluster_ram_size["data"]}"
-    index_ramsize                    = "${var.cluster_ram_size["index"]}"
-    fts_ramsize                      = "${var.cluster_ram_size["fts"]}"
-    services                         = "${join(",", var.services)}"
-    rally_autoscaling_group_id       = "${var.rally_autoscaling_group_id}"
+    region            = "${data.aws_region.current.name}"
+    apply_updates     = "${var.apply_updates ? "echo \"Applying updates...\"\nyum update -y": "echo \"Skipping updates\""}"
+    installer_url     = "${local.installer_url}"
+    couchbase_edition = "${var.couchbase_edition}"
+
+    topology                   = "${var.topology}"
+    cluster_name               = "${var.cluster_name}"
+    cluster_name_init          = "${local.major_version >= 5 ? var.cluster_name : ""}"
+    cluster_admin_username     = "${var.cluster_admin_username}"
+    cluster_admin_password     = "${var.cluster_admin_password}"
+    index_storage              = "${var.cluster_index_storage}"
+    data_ramsize               = "${var.cluster_ram_size["data"]}"
+    index_ramsize              = "${var.cluster_ram_size["index"]}"
+    fts_ramsize                = "${var.cluster_ram_size["fts"]}"
+    services                   = "${join(",", var.services)}"
+    rally_autoscaling_group_id = "${var.rally_autoscaling_group_id}"
 
     additional_initialization_script = "${var.additional_initialization_script}"
     auto_rebalance                   = "${var.auto_rebalance}"
